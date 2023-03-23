fc2e73f8b12438fec35e666115797cb285b11eeb
ARod88 <alma.rodriguez72@yahoo.com>
2023-03-22 23:57:24 -0700

    added fetch request

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
 async function main() {
-
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX:Huobi,TRP:TSX&interval=1min&apikey=d4e91f22c83e451bbd79fa8894b4a8b9.json');
+    let resultObject = response.json();
+    console.log(resultObject);
+    
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
 }
 
-main()
+ main()
+
